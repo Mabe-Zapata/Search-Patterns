d2e594fbcd338b1b3c8b2f84e6b4528a2957e2ff
kerly1811-mi <kerlychicaiza1811@gmail.com>
2026-04-19 20:23:30 -0500

    revert: restore original config files

--- SearchPatterns.API/Program.cs
+++ SearchPatterns.API/Program.cs
@@ -37,10 +37,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("AllowAll");
 
-if (!app.Environment.IsDevelopment())
-{
-	app.UseHttpsRedirection();
-}
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
